09ecfbbddd1deee497f08da1cd1a3081aec686db
alskill <fiazefaq@gmail.com>
2026-07-20 13:00:49 +0530

    Build first working Gemini AI application

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from config import GEMINI_API_KEY
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
+question = input("Ask your DevOps AI Agent: ")
+
 response = client.models.generate_content(
     model="gemini-flash-latest",
-    contents="Explain Kubernetes in simple words."
+    contents=question
 )
 
+print("\n🤖 AI Response:\n")
+print(response.text)
 print(response.text)
